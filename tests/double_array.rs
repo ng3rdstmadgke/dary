51063e51ad3955b92daf501232eaadfe486ccede
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2019-11-14 08:20:19 +0900

    テストを修正

--- tests/double_array.rs
+++ tests/double_array.rs
@@ -2,6 +2,9 @@ use dary::double_array::DoubleArray;
 use dary::trie::Trie;
 use rand::{Rng, thread_rng};
 use rand::distributions::Alphanumeric;
+use std::env;
+use std::fs;
+use std::path::PathBuf;
 
 #[test]
 fn double_array() {
@@ -15,9 +18,15 @@ fn double_array() {
 		trie.set(&key, i as u32);
 	}
 
-	let double_array: DoubleArray<u32> = trie.to_double_array().ok().unwrap();
+	let double_array: DoubleArray<u32> = trie.to_double_array().unwrap();
+
+	let mut path: PathBuf = env::current_dir().unwrap();
+	path.push("test.dic");
+	let double_array = double_array.dump(path.to_str().unwrap()).unwrap();
 
 	for (i, key) in keys.iter().enumerate() {
 		assert!(double_array.get(&key).unwrap().contains(&(i as u32)));
 	}
+
+	fs::remove_file(path).unwrap();
 }
